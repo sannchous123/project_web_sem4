955bbe4c25617893d42e5e79254dab5e5c1fab32
sannchous123 <sannchous123777@gmail.com>
2026-05-24 21:23:26 +0300

    Create Auth.php

---
+++ core/Auth.php
@@ -0,0 +1,35 @@
+<?php
+class Auth {
+    public static function generateUsername($full_name) {
+        $translit = [
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e',
+            'ё' => 'e', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k',
+            'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
+            'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts',
+            'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+        ];
+        $transliterated = strtr(mb_strtolower($full_name), $translit);
+        $transliterated = preg_replace('/[^a-z0-9]/', '_', $transliterated);
+        $transliterated = preg_replace('/_+/', '_', $transliterated);
+        $transliterated = trim($transliterated, '_');
+        return substr($transliterated, 0, 45) . '_' . substr(md5(uniqid()), 0, 4);
+    }
+
+    public static function generatePassword($length = 10) {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%';
+        $password = '';
+        for ($i = 0; $i < $length; $i++) {
+            $password .= $chars[random_int(0, strlen($chars) - 1)];
+        }
+        return $password;
+    }
+
+    public static function hashPassword($password) {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public static function verifyPassword($password, $hash) {
+        return password_verify($password, $hash);
+    }
+}
